e075b8889e8bbd492fdda89a92fce6daf0880a2e
Łukasz Szczęśniak <kontakt@lukaszszczesniak.com>
2022-06-21 04:12:16 +0200

    Add all CRUD endpoints for courses and articles

--- backend/simplifiedCoursesApp_backend/simplifiedCoursesApp_backend_api/urls.py
+++ backend/simplifiedCoursesApp_backend/simplifiedCoursesApp_backend_api/urls.py
@@ -5,14 +5,18 @@ from .views import (
     # TestModelListApiView,
     # TestModelDetailApiView,
     CourseModelListApiView,
-    ArticleModelListApiView
+    ArticleModelListApiView,
+    ArticleModelDetailsApiView,
+    CourseModelDetailApiView
 )
 
 urlpatterns = [
     # path('testmodels/', TestModelListApiView.as_view()),
     # path('testmodels/<int:testmodel_id>/', TestModelDetailApiView.as_view()),
     path('courses/', CourseModelListApiView.as_view()),
+    path('courses/<int:course_id>/', CourseModelDetailApiView.as_view()),
     path('articles/', ArticleModelListApiView.as_view()),
+    path('articles/<int:article_id>/', ArticleModelDetailsApiView.as_view()),
     # OpenAPI 3 documentation with Swagger UI:
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(template_name="swagger-ui.html", url_name="schema"), name="swagger-ui",)
--- backend/simplifiedCoursesApp_backend/simplifiedCoursesApp_backend_api/views.py
+++ backend/simplifiedCoursesApp_backend/simplifiedCoursesApp_backend_api/views.py
@@ -8,13 +8,13 @@ from .serializers import *
 
 
 class ArticleModelListApiView(APIView):
-    @extend_schema(responses=ArticleDtoSerializer)
+    @extend_schema(responses=ArticleDtoSerializer(many=True))
     def get(self, request, *args, **kwargs):
         articles_models = ArticleModel.objects
         response_serializer = ArticleDtoSerializer(articles_models, many=True)
         return Response(response_serializer.data, status=status.HTTP_200_OK)
 
-    @extend_schema(request=ArticleFormDtoSerializer, responses=ArticleDtoSerializer)
+    @extend_schema(request=ArticleFormDtoSerializer, responses={status.HTTP_201_CREATED: ArticleDtoSerializer})
     def post(self, request, *args, **kwargs):
         request_serializer = ArticleFormDtoSerializer(data=request.data)
         if request_serializer.is_valid():
@@ -29,15 +29,57 @@ class ArticleModelListApiView(APIView):
         return Response(request_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class ArticleModelDetailsApiView(APIView):
+    @extend_schema(responses={status.HTTP_200_OK: ArticleDtoSerializer, status.HTTP_400_BAD_REQUEST: str})
+    def get(self, request, article_id, *args, **kwargs):
+        article_model = None
+        try:
+            article_model = ArticleModel.objects.get(id=article_id)
+        except:
+            return Response("Article with given ID not found", status=status.HTTP_400_BAD_REQUEST)
+        response_serializer = ArticleDtoSerializer(article_model)
+        return Response(response_serializer.data, status=status.HTTP_200_OK)
+
+    @extend_schema(request=ArticleFormDtoSerializer, responses=ArticleDtoSerializer)
+    def put(self, request, article_id, *args, **kwargs):
+        article_model = None
+        try:
+            article_model = ArticleModel.objects.get(id=article_id)
+        except:
+            return Response("Article with given ID not found", status=status.HTTP_400_BAD_REQUEST)
+        request_serializer = ArticleFormDtoSerializer(data=request.data)
+        if request_serializer.is_valid():
+            article_model_serializer = ArticleModelSerializer(instance=article_model, data=request_serializer.data,
+                                                              partial=True)
+            if article_model_serializer.is_valid():
+                article_model_serializer.save()
+                response_serializer = ArticleDtoSerializer(data=article_model_serializer.data)
+                if response_serializer.is_valid():
+                    return Response(response_serializer.data, status=status.HTTP_200_OK)
+                return Response(response_serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(article_model_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(request_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(responses={status.HTTP_200_OK: str, status.HTTP_400_BAD_REQUEST: str})
+    def delete(self, request, article_id, *args, **kwargs):
+        article_model = None
+        try:
+            article_model = ArticleModel.objects.get(id=article_id)
+        except:
+            return Response("Article with given ID not found", status=status.HTTP_400_BAD_REQUEST)
+        article_model.delete()
+        return Response("Article deleted", status=status.HTTP_200_OK)
+
+
 class CourseModelListApiView(APIView):
-    @extend_schema(responses=CourseDtoSerializer)
+    @extend_schema(responses=CourseDtoSerializer(many=True))
     def get(self, request, *args, **kwargs):
         courses_models = CourseModel.objects
         serializer = CourseModelSerializer(courses_models, many=True)
         response_serializer = CourseDtoSerializer(serializer.data, many=True)
         return Response(response_serializer.data, status=status.HTTP_200_OK)
 
-    @extend_schema(request=CourseFormDtoSerializer, responses=CourseDtoSerializer)
+    @extend_schema(request=CourseFormDtoSerializer, responses={status.HTTP_201_CREATED: CourseDtoSerializer})
     def post(self, request, *args, **kwargs):
         request_serializer = CourseFormDtoSerializer(data=request.data)
         if request_serializer.is_valid():
@@ -51,6 +93,48 @@ class CourseModelListApiView(APIView):
             return Response(course_model_serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return Response(request_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CourseModelDetailApiView(APIView):
+    @extend_schema(responses={status.HTTP_200_OK: CourseDtoSerializer, status.HTTP_400_BAD_REQUEST: str})
+    def get(self, request, course_id, *args, **kwargs):
+        course_model = None
+        try:
+            course_model = CourseModel.objects.get(id=course_id)
+        except:
+            return Response("Course with given ID not found", status=status.HTTP_400_BAD_REQUEST)
+        response_serializer = CourseDtoSerializer(course_model)
+        return Response(response_serializer.data, status=status.HTTP_200_OK)
+
+    @extend_schema(request=CourseFormDtoSerializer, responses=CourseDtoSerializer)
+    def put(self, request, course_id, *args, **kwargs):
+        course_model = None
+        try:
+            course_model = CourseModel.objects.get(id=course_id)
+        except:
+            return Response("Course with given ID not found", status=status.HTTP_400_BAD_REQUEST)
+        request_serializer = CourseFormDtoSerializer(data=request.data)
+        if request_serializer.is_valid():
+            course_model_serializer = CourseModelSerializer(instance=course_model, data=request_serializer.data,
+                                                              partial=True)
+            if course_model_serializer.is_valid():
+                course_model_serializer.save()
+                response_serializer = CourseDtoSerializer(data=course_model_serializer.data)
+                if response_serializer.is_valid():
+                    return Response(response_serializer.data, status=status.HTTP_200_OK)
+                return Response(response_serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(course_model_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(request_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(responses={status.HTTP_200_OK: str, status.HTTP_400_BAD_REQUEST: str})
+    def delete(self, request, course_id, *args, **kwargs):
+        course_model = None
+        try:
+            course_model = CourseModel.objects.get(id=course_id)
+        except:
+            return Response("Course with given ID not found", status=status.HTTP_400_BAD_REQUEST)
+        course_model.delete()
+        return Response("Course deleted", status=status.HTTP_200_OK)
 #
 #
 # class TestModelListApiView(APIView):
